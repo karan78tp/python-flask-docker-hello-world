5bc114879ac339bfa8a705ca4d7576a376ea69cb
krank <karan78tp@gmail.com>
2018-05-29 22:19:34 +0530

    DSesdewdsed

--- site/flask_program.py
+++ site/flask_program.py
@@ -1,13 +1,12 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def hello():
-#     return "Flask inside Docker!!"
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True,host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
 
-print ("hello ")
